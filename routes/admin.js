4227ff4eae1afe6e116491840cdd56c42219f002
Heo <cyts1001@ajou.ac.kr>
2017-12-03 17:04:50 +0900

    no message

---
+++ routes/admin.js
@@ -0,0 +1,161 @@
+var express = require('express');
+var crypto = require('crypto');
+var bodyParser = require('body-parser');
+var router = express.Router();
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+    host     : '13.124.255.163',
+    user     : 'root',
+    password : 'rPtjdgurdldnfkagjdnd123',
+    database : 'ascc'
+});
+connection.connect();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    res.render('admin/login');
+});
+
+router.post('/login', function(req,res,next){
+   res.json({"code" : 200})
+});
+//TODO login 후 교수님 세션 가져오기
+router.get('/index', function(req, res, next) {
+    connection.query('SELECT * from class_info', function (error, results, fields) {
+        if (error)
+            res.json("error");
+
+        console.log(results);
+        res.render('admin/index',{classes: results});
+    });
+
+});
+
+router.get('/class/:id', function(req, res, next) {
+    var id = req.params.id;
+    connection.query('select * from student_info, ci_si_relation, class_info where si_type="student" and csr_ci_id="'+ id +'" and csr_si_id= si_id and ci_id="'+id+'";', function (error, results, fields) {
+        if (error)
+            res.json("error");
+
+        console.log(results[0]);
+        for(var i=0; i<results.length; i++){
+            results[i].attendance = Math.floor(Math.random() * (40 - 35 + 1)) + 35;
+            results[i].absence = 40-results[i].attendance;
+            results[i].breakaway = Math.floor((Math.random() * 10) + 1);
+
+        }
+
+        res.render('admin/class',{studnets: results});
+    });
+
+});
+
+router.get('/class/:ci_code/:si_num', function(req, res, next) {
+    var ci_code = req.params.ci_code;
+    var si_num = req.params.si_num;
+    connection.query('select * from attendance \n' +
+        'where si_num = "'+si_num+'"\n' +
+        'and ci_code = "'+ci_code+'"', function (error, results, fields) {
+        if (error)
+            res.json("error");
+        //개강일 -> 20170901
+        var str_date = new Date("2017-09-01");
+
+        //종강일 -> 20171222
+        var end_date = new Date("2017-12-22");
+
+        //수업 요일 및 강의실
+        var ci_week = "Mon 10:30~12:00 Pal 410,Mon 13:00~15:00 Pal 333 Pal 318,Thur 10:30~12:00 Pal 410";
+        ci_week = ci_week.split(',');
+        for(var i = 0; i<ci_week.length; i++){
+            ci_week[i] = ci_week[i].split(' ');
+        }
+        //날짜계산
+        var date_arr = [];
+        var i = 0;
+        var cnt = 0;
+        while(1){
+            if(i==0){
+                for(var j = 0; j<ci_week.length; j++){
+                    if(ci_week[j][0] == str_date.format("E")){
+                        if(ci_week[j][0] == str_date.format("E")){
+                            date_arr[cnt] = str_date.format("yyyy-MM-dd")+" "+ ci_week[j][1] +" "+ ci_week[j][2] +""+ ci_week[j][3];
+                            date_arr[cnt] = date_arr[cnt].split(" ");
+                            date_arr[cnt][3] = 0;
+                            date_arr[cnt][4] = 0;
+                            cnt++;
+                        }
+                    }
+                }
+
+            }else{
+                var dayOfMonth = str_date.getDate();
+                str_date.setDate(dayOfMonth + 1);
+                for(var j = 0; j<ci_week.length; j++){
+                    if(ci_week[j][0] == str_date.format("E")){
+                        date_arr[cnt] = str_date.format("yyyy-MM-dd")+" "+ ci_week[j][1] +" "+ ci_week[j][2] +""+ ci_week[j][3];
+                        date_arr[cnt] = date_arr[cnt].split(" ");
+                        date_arr[cnt][3] = 0;
+                        date_arr[cnt][4] = 0;
+                        cnt++;
+                    }
+                }
+
+                if(str_date.toString() === end_date.toString()){
+                    break;
+                }
+            }
+            i++;
+        }
+        var attendance;
+        for(var i=0; i<2; i++){
+            var str_time = date_arr[i][1].split('~')[0];
+            var end_time = date_arr[i][1].split('~')[1];
+            console.log("str_time :" +str_time);
+            console.log("end_time :" +end_time);
+            for(var j=0; j<results.length; j++){
+                if(date_arr[i][0] == results[j].created_at.format('yyyy-MM-dd')){
+                    if(str_time < results[j].created_at.format('HH:mm') && end_time > results[j].created_at.format('HH:mm')){
+                        date_arr[i][3]++;
+                        if(date_arr[i][4] == 0)
+                            date_arr[i][4] = results[j].created_at.format('yyyy-MM-dd HH:mm');
+                    }
+                }
+            }
+        }
+         console.log(date_arr);
+
+        res.render('admin/student',{attendance: results, date_arr: date_arr});
+    });
+
+});
+
+Date.prototype.format = function(f) {
+    if (!this.valueOf()) return " ";
+
+    var weekName = ["Sun", "Mon", "Tue", "Wes", "Thur", "Fri", "Sat"];
+    var d = this;
+
+    return f.replace(/(yyyy|yy|MM|dd|E|hh|mm|ss|a\/p)/gi, function($1) {
+        switch ($1) {
+            case "yyyy": return d.getFullYear();
+            case "yy": return (d.getFullYear() % 1000).zf(2);
+            case "MM": return (d.getMonth() + 1).zf(2);
+            case "dd": return d.getDate().zf(2);
+            case "E": return weekName[d.getDay()];
+            case "HH": return d.getHours().zf(2);
+            case "hh": return ((h = d.getHours() % 12) ? h : 12).zf(2);
+            case "mm": return d.getMinutes().zf(2);
+            case "ss": return d.getSeconds().zf(2);
+            case "a/p": return d.getHours() < 12 ? "오전" : "오후";
+            default: return $1;
+        }
+    });
+};
+
+String.prototype.string = function(len){var s = '', i = 0; while (i++ < len) { s += this; } return s;};
+String.prototype.zf = function(len){return "0".string(len - this.length) + this;};
+Number.prototype.zf = function(len){return this.toString().zf(len);};
+
+module.exports = router;
+
---
+++ routes/api.js
@@ -0,0 +1,190 @@
+var express = require('express');
+var crypto = require('crypto');
+var router = express.Router();
+
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+    host     : '13.124.255.163',
+    user     : 'root',
+    password : 'rPtjdgurdldnfkagjdnd123',
+    database : 'ascc'
+});
+connection.connect();
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    res.render('index', { title: 'api_controller' });
+});
+//test
+router.get('/getClass', function(req, res, next) {
+
+    connection.query('SELECT * from class_info', function (error, results, fields) {
+        if (error)
+            res.json("error");
+        // console.log('The solution is: ', Date.now());
+        res.json({"code" : 200, "response" : "success", "data" : results});
+    });
+});
+
+router.get('/getClassAuth', function(req,res,next){
+    var si_num = req.query.si_num;
+
+    if(si_num == '' || si_num == undefined){
+        res.json({"code" : 203, "response" : "empty parmater"});
+        return false;
+    }
+
+    var result_data;
+
+    UserInfo(req, function(ret_json){
+        if(ret_json.code != 200){
+            res.json(ret_json);
+            return false;
+        }
+        result_data={"code": 200 ,"user_info" : ret_json.data};
+        ClassAuth(ret_json.data, function(ret_json){
+            if(ret_json.code != 200){
+                res.json(ret_json);
+                return false;
+            }
+            result_data.auth_data = ret_json;
+            res.json(result_data);
+        });
+    });
+});
+
+function UserInfo(req, callback){
+
+    connection.query("select * from student_info where si_num='"+req.query.si_num+"'", function(err, res, fields){
+        if(err){
+            callback({"code" : 202, "response" : "sql_err", "data" : err});
+            return err;
+        }
+       callback({"code" : 200, "response" : "success", "data" : res[0]});
+       return res;
+    });
+}
+
+function ClassAuth(req, callback){
+    if(req == undefined){
+        callback({"code" : 202, "response" : "empty sql select"});
+        return false;
+    }
+
+    connection.query('SELECT * from class_info where ci_prof_id="'+req.si_id+'"', function (error, results, fields) {
+        if (error){
+            callback({"code" : 202, "response" : "msyql_error", "error" : error});
+            return false;
+        }
+
+        var time = new Date();
+        var auth = crypto.createHash('md5').update(time.toTimeString()).digest('hex');
+        auth = auth.insert(8,'-');
+        auth = auth.insert(13,'-');
+        auth = auth.insert(18,'-');
+        auth = auth.insert(23,'-');
+
+        if(results == ''){
+            callback({"code" : 201, "response" : "empty_Class"});
+            return false;
+        }else{
+            connection.query("insert into attendance_info (ai_ci_code, ai_si_id, ai_auth) values ('"+results[0].ci_code+"', '"+req.si_id+"','"+auth+"')", function (error, ins_results,fileds){
+                if (error){
+                    callback({"code" : 202, "response" : "msyql_error", "error" : error});
+                    return false;
+                }
+            });
+        }
+        callback({"code" : 200, "response" : "success", "data" : results, "auth" : auth});
+    });
+}
+//TODO 이메일 인증보내기
+router.post('/postAuth', function(req, res, next) {
+
+    connection.query('SELECT * from class_info', function (error, results, fields) {
+        if (error)
+            res.json({"code" : 202, "response" : "msyql_error", "error" : error});
+        // console.log('The solution is: ', results);
+        res.json({"code" : 200, "response" : "success", "data" : results});
+    });
+
+});
+//TODO 로그인
+router.post('/login',function(req,res){
+    //validate
+    var email = req.body.si_email;
+    var pwd = req.body.si_pwd;
+
+    console.log(email);
+    console.log(pwd);
+
+   connection.query('select * from student_info where si_email = "'+email+'" and si_pwd = "'+pwd+'"', function(error, results, fields){
+       if (error)
+           res.json({"code" : 202, "response" : "msyql_error", "error" : error});
+       // console.log('The solution is: ', results);
+       res.json({"code" : 200, "response" : "success", "data" : results});
+   });
+});
+//TODO 교수님 확인
+//TODO 수업체크
+
+//TODO 출석인증
+router.post('/getAtdChk', function(req, res, next) {
+    //validate
+    var si_num = req.body.stdId;
+    var ci_code = req.body.ciCode;
+    var auth = req.body.auth;
+
+    console.log(ci_code);
+
+    if(si_num == undefined|| ci_code == undefined || auth == undefined){
+        res.send({"code" : 203, "response" : "empty parmater"});
+    }
+    var queryString = "SELECT * FROM attendance_info WHERE ai_ci_code ='"+ ci_code +"' ORDER BY ai_id DESC LIMIT 1";
+    connection.query(queryString, function (error, results) {
+
+        var ai_auth =results[0].ai_auth;
+
+        if(ai_auth == auth) {
+            queryString = "INSERT INTO attendance (ci_code, si_num, auth) VALUES('"+ ci_code +"', '"+ si_num +"','"+ ai_auth +"')";
+            connection.query(queryString, function (error, results) {
+                console.log(results);
+
+                if(error)
+                    res.send({"code" : 202, "response" : "msyql_error", "error" : error});
+                else if(results)
+                    res.send({"code" : 200, "response" : "attendace check success", "data" : results});
+            });
+        }
+    });
+
+    return true;
+});
+
+//TODO 수업리스트
+router.post('/getStuClass', function (req, res, next) {
+    var si_num = req.body.stdId;
+    var queryString = 'SELECT a.si_num, a.si_name, c.ci_name, c.ci_room, c.ci_time FROM ascc.student_info AS a left join ascc.ci_si_relation as b ON a.si_id = b.csr_si_id inner join ascc.class_info AS c ON b.csr_ci_id = c.ci_id WHERE a.si_num ='+si_num+';';
+    // console.log(si_num);
+    connection.query(queryString, function (error, results) {
+        // console.log(results);
+        if(error)
+            res.send({"code" : 202, "response" : "msyql_error", "error" : error});
+        else if(results.length > 0)
+            res.send({"code" : 200, "response" : "success", "data" : results});
+        else if(results)
+            res.send({"code" : 200, "response" : "this student has no class", "data" : results});
+    });
+
+    return true;
+});
+
+module.exports = router;
+
+String.prototype.insert = function (index, string) {
+    if (index > 0)
+        return this.substring(0, index) + string + this.substring(index, this.length);
+    else
+        return string + this;
+};
